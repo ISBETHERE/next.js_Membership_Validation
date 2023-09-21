486f4385e7eaf72935d084bac66d68100f5dd18a
ISBETHERE <oksky2957@daum.net>
2023-09-21 09:18:17 +0900

    8

--- next.config.js
+++ next.config.js
@@ -2,11 +2,30 @@
 module.exports = {
     // Use the prefix for asset path
     assetPrefix: process.env.NODE_ENV === 'production' ? '/next.js_Membership_Validation' : '',
-    output: 'export',
-    rules: {
-        // 기존 규칙...
+    // output: 'export',
+    // rules: {
+    //     // 기존 규칙...
+    //
+    //     // Prettier 규칙을 비활성화
+    //     'prettier/prettier': 'off',
+    // },
 
-        // Prettier 규칙을 비활성화
-        'prettier/prettier': 'off',
+    experimental: {
+        appDir: true,
+    },
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(wav)$/,
+            use: {
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                    publicPath: `/_next/static/sounds/`,
+                    outputPath: `${options.isServer ? "../" : ""}static/sounds/`,
+                },
+            },
+        });
+
+        return config;
     },
 }
